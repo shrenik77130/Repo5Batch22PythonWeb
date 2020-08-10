1920bde9e065499afd6c9299a38aead8af2d9f59
shrenik77130 <shrenik77130@gmail.com>
2020-08-10 12:42:54 +0530

    Python If Else Assignments

---
+++ #4_IfElse_ConditionChecking/Assignments.py
@@ -0,0 +1,14 @@
+'''
+Ass1: WAP to input any character and check that entered character is Capital Alphabate or 
+Small Alphabate, any digit or any symbol
+
+Ass2: WAP to input gender and age of an employee. check that employee is elegible for insuarance
+or not in following cases
+    a. If employee is male and age is greater than 25
+    b. If employee is female and age is greater than 21
+    
+Ass3: WAP to input any 3 digit number and check that number is Armstrong number or not.
+e.g. 153 = 1*1*1 + 5*5*5 + 3*3*3   
+If addition of cube of saperate digits in a number is same as input number then it is called as 
+"Armstrong Number"
+'''
