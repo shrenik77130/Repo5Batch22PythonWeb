de70129e1f3c8d1fc31bf835789237950597fee7
shrenik77130 <shrenik77130@gmail.com>
2020-08-11 12:45:07 +0530

    Operators Examples

---
+++ #5_Operators/OperatorEx1.py
@@ -0,0 +1,58 @@
+#Program1
+a=10
+b=5
+
+ans=a+b/a-b
+print("a=%d \t b=%d \t ans=%d"%(a,b,ans))
+
+# Program2
+a=6
+b=4
+
+ans=a%b+b%a-5%(a*b)
+print("a=%d \t b=%d \t ans=%d"%(a,b,ans))
+
+#Program3
+a=15
+a+=5 #a=a+5
+
+b=30
+b-=3 #b=b-3
+
+ans=a//b*b//a
+print("a=%d \t b=%d \t ans=%d"%(a,b,ans))
+
+"""
+Assignment
+
+#pr1
+x=8
+y=30
+z=x+y-(10%x)*(y+20)
+
+ans=x**y-z
+What is ans?
+
+#prg2
+s=t=p=q=5
+
+if(s==p):
+    t=q=10
+else
+    t=q=15
+    
+if(s==5):
+    if(t!=5):
+        s=t=p=q=35
+        
+ans=s-q*q/t+p
+what is ans?
+
+#pr3
+w=99
+ww=999
+www=9999
+
+ans=www-(w+10)-ww-(www-99)
+what is ans?
+"""
